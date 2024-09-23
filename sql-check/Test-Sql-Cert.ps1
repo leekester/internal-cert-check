be47b4aa9b4bc8d9a8834ef293a5c0e2e5e41aac
leekester <58551952+leekester@users.noreply.github.com>
2024-09-23 20:17:17 +0100

    Adding retrieval of subject name and days to expiry

--- sql-check/Test-Sql-Cert.ps1
+++ sql-check/Test-Sql-Cert.ps1
@@ -1,4 +1,3 @@
-
 using namespace System.Net.Sockets
 using namespace System.Net.Security
 using namespace System.Security.Cryptography.X509Certificates
@@ -36,9 +35,11 @@ ForEach ($endpoint in $endpoints) {
         $tempObject | Add-Member -MemberType NoteProperty -Name dnsName -Value $endpoint.dnsName
         $tempObject | Add-Member -MemberType NoteProperty -Name port -Value $endpoint.port
         $tempObject | Add-Member -MemberType NoteProperty -Name connectionState -Value "Failed"
-        $tempObject | Add-Member -MemberType NoteProperty -Name validUntil -Value "Unknown"
-        $tempObject | Add-Member -MemberType NoteProperty -Name issuer -Value "Unknown"
+        $tempObject | Add-Member -MemberType NoteProperty -Name subject -Value "Unknown"
         $tempObject | Add-Member -MemberType NoteProperty -Name sans -Value "Unknown"
+        $tempObject | Add-Member -MemberType NoteProperty -Name validUntil -Value "Unknown"
+        $tempObject | Add-Member -MemberType NoteProperty -Name daysToExpiry -Value "Unknown"
+        $tempObject | Add-Member -MemberType NoteProperty -Name issuer -Value "Unknown"
         $certResults += $tempObject
         Continue
     }
@@ -59,13 +60,18 @@ ForEach ($endpoint in $endpoints) {
         $tlsClient.Dispose()
         }
     }
+
+    $daysToExpiry = ($result.NotAfter - (Get-Date)).days
+
     $tempObject = New-Object PSObject
     $tempObject | Add-Member -MemberType NoteProperty -Name dnsName -Value $endpoint.dnsName
     $tempObject | Add-Member -MemberType NoteProperty -Name port -Value $endpoint.port
     $tempObject | Add-Member -MemberType NoteProperty -Name connectionState -Value "Successful"
-    $tempObject | Add-Member -MemberType NoteProperty -Name validUntil -Value $result.NotAfter
-    $tempObject | Add-Member -MemberType NoteProperty -Name issuer -Value $result.IssuerName.Name
+    $tempObject | Add-Member -MemberType NoteProperty -Name subject -Value $result.subject.Split(",")[0].split("`=")[1]
     $tempObject | Add-Member -MemberType NoteProperty -Name sans -Value ($result.Extensions | Where-Object {$_.Oid.FriendlyName -eq "Subject Alternative Name"}).format($true)
+    $tempObject | Add-Member -MemberType NoteProperty -Name validUntil -Value $result.NotAfter
+    $tempObject | Add-Member -MemberType NoteProperty -Name daysToExpiry -Value $daysToExpiry
+    $tempObject | Add-Member -MemberType NoteProperty -Name issuer -Value $result.IssuerName.Name
     $certResults += $tempObject
 }   
 
