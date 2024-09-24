9415f3d77be03058073cbb2033634979eafe7ce2
leekester <58551952+leekester@users.noreply.github.com>
2024-09-24 09:50:05 +0100

    Adding routing to test socket is listening for performance improvement

--- sql-check/Test-Sql-Cert.ps1
+++ sql-check/Test-Sql-Cert.ps1
@@ -2,18 +2,22 @@ using namespace System.Net.Sockets
 using namespace System.Net.Security
 using namespace System.Security.Cryptography.X509Certificates
 
+# Define variables
+$outputFile = ("output_" + (Get-Date -Format ddMMyyyy_HHmmss) + ".csv")
+$inputFile = (".\endpoints.csv")
+
 # Define function
 function ConvertFrom-X509Certificate {
     param(
         [Parameter(ValueFromPipeline)]
-        [X509Certificate2]$Certificate
+        [X509Certificate2]$certificate
     )
 
     process {
         @(
             '-----BEGIN CERTIFICATE-----'
             [Convert]::ToBase64String(
-                $Certificate.Export([X509ContentType]::Cert),
+                $certificate.export([X509ContentType]::Cert),
                 [Base64FormattingOptions]::InsertLineBreaks
             )
             '-----END CERTIFICATE-----'
@@ -27,8 +31,26 @@ $certResults = @()
 
 # Loop through endpoints, performing TLS handshake
 ForEach ($endpoint in $endpoints) {
+    $tempObject = $null
     Try {
-        $tcpClient = [TcpClient]::new($endpoint.dnsName, $endpoint.port)
+        Write-Host "Attempting creation of socket to $($endpoint.dnsName)"
+        $listening = (New-Object System.Net.Sockets.TcpClient).ConnectAsync($endpoint.dnsName, $endpoint.port).Wait(500)
+        If (-Not $listening) {
+            $tempObject = New-Object PSObject
+            $tempObject | Add-Member -MemberType NoteProperty -Name dnsName -Value $endpoint.dnsName
+            $tempObject | Add-Member -MemberType NoteProperty -Name port -Value $endpoint.port
+            $tempObject | Add-Member -MemberType NoteProperty -Name connectionState -Value "Failed"
+            $tempObject | Add-Member -MemberType NoteProperty -Name subject -Value "Unknown"
+            $tempObject | Add-Member -MemberType NoteProperty -Name sans -Value "Unknown"
+            $tempObject | Add-Member -MemberType NoteProperty -Name validUntil -Value "Unknown"
+            $tempObject | Add-Member -MemberType NoteProperty -Name daysToExpiry -Value "Unknown"
+            $tempObject | Add-Member -MemberType NoteProperty -Name issuer -Value "Unknown"
+            $tempObject | Add-Member -MemberType NoteProperty -Name description -Value $endpoint.description
+            $certResults += $tempObject
+            Continue
+        }
+        Write-Host "Finished trying creation of socket"
+        
     }
     Catch {
         $tempObject = New-Object PSObject
@@ -40,11 +62,13 @@ ForEach ($endpoint in $endpoints) {
         $tempObject | Add-Member -MemberType NoteProperty -Name validUntil -Value "Unknown"
         $tempObject | Add-Member -MemberType NoteProperty -Name daysToExpiry -Value "Unknown"
         $tempObject | Add-Member -MemberType NoteProperty -Name issuer -Value "Unknown"
+        $tempObject | Add-Member -MemberType NoteProperty -Name description -Value $endpoint.description
         $certResults += $tempObject
         Continue
     }
 
     Try {
+        $tcpClient = [TcpClient]::new($endpoint.dnsName, $endpoint.port)
         $tlsClient = [SslStream]::new($tcpClient.GetStream())
         $tlsClient.AuthenticateAsClient($endpoint.dnsName)
 
@@ -72,7 +96,8 @@ ForEach ($endpoint in $endpoints) {
     $tempObject | Add-Member -MemberType NoteProperty -Name validUntil -Value $result.NotAfter
     $tempObject | Add-Member -MemberType NoteProperty -Name daysToExpiry -Value $daysToExpiry
     $tempObject | Add-Member -MemberType NoteProperty -Name issuer -Value $result.IssuerName.Name
+    $tempObject | Add-Member -MemberType NoteProperty -Name description -Value $endpoint.description
     $certResults += $tempObject
 }   
 
-$certResults | Export-Csv .\outputs.csv -UseCulture -NoTypeInformation
+$certResults | Export-Csv $outputFile -UseCulture -NoTypeInformation
